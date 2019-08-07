43b553e7102d97d10b315cb8aa477dab643c0b01
Conor Sheehan <ConorJamesSheehan@gmail.com>
2019-08-07 16:59:19 +0100

    bugfix

--- pubmed_text_classification/model/lstm.py
+++ pubmed_text_classification/model/lstm.py
@@ -12,7 +12,7 @@ class TransitionMatrix(nn.Module):
         self.transition_probabilities = nn.Linear(1, output_dim, bias=False)
 
     def forward(self, previous_label):
-        return self.transition_probabilities(previous_label)
+        return self.transition_probabilities(previous_label.unsqueeze(1))
 
 
 class NewModel(nn.Module):
@@ -22,11 +22,14 @@ class NewModel(nn.Module):
         super().__init__()
         word2vec = gensim.models.KeyedVectors.load_word2vec_format(pretrained_weights, binary=True)
         self.word_to_ix = {word: ix for ix, word in enumerate(word2vec.index2word)}
+        # self.word_to_ix = {}
         weights = torch.FloatTensor(word2vec.vectors)
+        # weights = torch.zeros(1, 100)
         self.embedding = nn.Embedding.from_pretrained(weights)
-        self.lstm = nn.LSTM(input_size=weights.shape[1], hidden_size=hidden_dim, bidirectional=True,
+
+        self.lstm = nn.LSTM(input_size=weights.shape[1], hidden_size=hidden_dim//2, bidirectional=True,
                             num_layers=lstm_layers)
-        self.drop = nn.Dropout(dropout)
+        self.dropout = nn.Dropout(dropout)
         self.fc_layer = nn.Linear(hidden_dim, output_dim)
 
         self.transition_matrix = TransitionMatrix(output_dim)
@@ -34,11 +37,14 @@ class NewModel(nn.Module):
     def forward(self, X):
         sentence, last_label = X
         tokens = torch.nn.utils.rnn.pad_sequence(
-            [torch.tensor([self.word_to_ix[word] for word in word_tokenize(s.lower())]) for s in sentence])
+            [torch.tensor([self.word_to_ix.get(word, 0) for word in word_tokenize(s.lower())], dtype=torch.int64)
+             for s in sentence])
         vec = self.embedding(tokens)
-        lstm_out = self.lstm(vec)
-        dropout = self.dropout(lstm_out)
-        fc_out = F.relu(self.fc_layer(dropout))
+        drop = self.dropout(vec)
+
+        lstm_out, (h, c) = self.lstm(drop)
+        h = torch.cat([h[-2, :, :], h[-1, :, :]], dim=1)
+        fc_out = F.relu(self.fc_layer(h))
         transition_out = self.transition_matrix(last_label)
 
         logits = fc_out + transition_out
@@ -47,8 +53,23 @@ class NewModel(nn.Module):
 
 if __name__ == '__main__':
     import os
+    from torch.utils.data import Dataset, DataLoader
     pretrained_weights = os.path.join('..', '..', 'pretrained_embeddings',
                                       'word2vec', 'wikipedia-pubmed-and-PMC-w2v.bin')
-    # model = NewModel(pretrained_weights, output_dim=5)
-    pass
+    model = NewModel(pretrained_weights, output_dim=5)
+    last_label = torch.FloatTensor([0, 1, 2, 3])
+    sentences = ['oxygen carbon silicon', 'dog cat mouse', 'flu cancer', 'biology chemistry']
+    labels = [1, 2, 3, 4]
 
+    class MyDataSet(Dataset):
+
+        def __len__(self):
+            return 4
+
+        def __getitem__(self, i):
+            return [sentences[i], last_label[i]], labels[i]
+
+
+    dataloader = DataLoader(MyDataSet(), batch_size=2)
+    for X, y in dataloader:
+        yhat = model(X)
