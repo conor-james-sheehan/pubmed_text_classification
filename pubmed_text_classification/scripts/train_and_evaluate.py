024a15f029edfaa3f3788f04bf2e37f52716a3fd
Conor Sheehan <ConorJamesSheehan@gmail.com>
2019-08-09 12:43:46 +0100

    bugfix

--- pubmed_text_classification/scripts/train_and_evaluate.py
+++ pubmed_text_classification/scripts/train_and_evaluate.py
@@ -1,28 +1,31 @@
 from argparse import ArgumentParser
 import sys
 import os
-sys.path += list(map(os.path.abspath, ['..', '../..', '../../..']))  # TODO: actually figure out how python paths work
+sys.path += list(map(os.path.abspath, ['..', '../..', '../../..']))
 from pubmed_text_classification.train import train
 from pubmed_text_classification.datasets import SupplementedAbstractSentencesDataset
 from pubmed_text_classification.evaluate import evaluate
 from pubmed_text_classification.model import TransitionModelConfig
 
 parser = ArgumentParser()
-parser.add_argument('--train_path', default=None,
+parser.add_argument('--train_path', default=None, type=str,
                     help='Path to csv file to use for training dataset. '
                          'If unspecified, will use the pubmed20k dataset.')
-parser.add_argument('--test_path', default=None,
+parser.add_argument('--test_path', default=None, type=str,
                     help='Path to csv file to use for test dataset. '
                          'If unspecified, will use the pubmed20k dataset.')
-parser.add_argument('--pretrained_weights', default='../pretrained_embeddings/wikipedia-pubmed-and-PMC-w2v.bin',
+parser.add_argument('--pretrained_embeddings', type=str,
+                    default='../pretrained_embeddings/wikipedia-pubmed-and-PMC-w2v.bin',
                     help='Path to file containing pretrained word2vec weights in binary format.')
-parser.add_argument('--pretrained_model', default=None, help='Path to binary save file of a previously trained model. '
-                                                             'If not specified, creates a new model to use.')
-parser.add_argument('--savedir', default='../results', help='Path to directory wherein to save results and models.')
-parser.add_argument('--n_epochs', default=100, help='Number of epochs to train for.')
-parser.add_argument('--batch_size', default=256)
-parser.add_argument('--valid_split', default=0.2, help='Fraction of data to use for validation.')
-parser.add_argument('--lr', default=0.01, help='Learning rate')
+parser.add_argument('--pretrained_model', default=None, type=str,
+                    help='Path to binary save file of a previously trained model. '
+                         'If not specified, creates a new model to use.')
+parser.add_argument('--savedir', default='../results', type=str,
+                    help='Path to directory wherein to save results and models.')
+parser.add_argument('--n_epochs', default=100, type=int, help='Number of epochs to train for.')
+parser.add_argument('--batch_size', default=256, type=int)
+parser.add_argument('--valid_split', default=0.2, type=float, help='Fraction of data to use for validation.')
+parser.add_argument('--lr', default=0.01, type=float, help='Learning rate')
 
 
 def main():
